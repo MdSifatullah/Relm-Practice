3e2eb395298d7f6c35bfd00d2a30c221b03632e4
Md Sifat <cse.sifat@gmail.com>
2020-01-05 15:44:16 +0600

    UIAlert, Realm, Data input in alert textFields and save then show in table.

---
+++ Relm Practice/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  Relm Practice
+//
+//  Created by Md Sifat on 1/2/20.
+//  Copyright © 2020 Md Sifat. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object {
+    @objc dynamic var taskName = ""
+    //@objc dynamic var taskDone = false
+}
--- Relm Practice/ViewController.swift
+++ Relm Practice/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import RealmSwift
+import Foundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var realm : Realm!
+    
+    var taskList : Results<Task>{
+        get{
+            return realm.objects(Task.self)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        realm = try! Realm()
+        print(realm.configuration.fileURL)
+        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = taskList[indexPath.row]
+        cell.textLabel?.text = item.taskName
+        print("\(item.taskName)")
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    
+    
+    @IBAction func onClickAdd(_ sender: UIButton) {
+        
+        let newTask = Task()
+        
+        let alert = UIAlertController(title: "Create New List", message: "Add new item", preferredStyle: .alert)
+        alert.addTextField{ (UITextField) in
+            UITextField.placeholder = "Enter text ..."
+        }
+        
+        let cancelAlert = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alert.addAction(cancelAlert)
+        let saveAlert = UIAlertAction(title: "Save", style: .default, handler: { (UIAlertAction) in
+            
+            let newTaskText = alert.textFields?.first as! UITextField
+            print("Check: \(newTaskText)")
+            newTask.taskName = newTaskText.text!
+            try! self.realm.write {
+                self.realm.add(newTask)
+                self.tableView.insertRows(at: [IndexPath.init(row: self.taskList.count - 1, section: 0)], with: .automatic)
+                print("Saved")
+                
+            }
+        })
+        alert.addAction(saveAlert)
+        present(alert, animated: true)
+    }
 }
 
